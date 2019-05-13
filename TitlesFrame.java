d691a1b66d29d5a1365c0c648aef516d2b4f1c7f
galcev <dengalcev@gmail.com>
2019-05-13 22:37:24 +0300

    TitlesFrame
    Добавление документации

--- TitlesFrame.java
+++ TitlesFrame.java
@@ -1,4 +1,29 @@
+/**
+ * @author Galcev
+ * @versin 1.0
+ * 
+ * 
+ *This file contains the title of the program. Sets window sizes and launch.
+ * This file contains the creation A collection of utility methods for Swing
+ */
+
 import javax.swing.JFrame;
+
+/**
+ *setTitle
+ *public void setTitle(String title)
+ *(Sets the title for this frame to the specified string.)
+ *@Param: title - the title to be displayed in the frame's border. A null value is treated as an empty string, "".
+ *@see: getTitle()
+ * 
+ *add
+ *public Component add(Component comp)
+ *
+ *setSize
+ *public void setSize(int width, int height)
+ *@param: width - the new width of this component in pixelsheight - the new height of this component in pixels
+ *
+ */
 
 public class TitlesFrame extends JFrame
 {
