c0d68233d356a4ee55d9d3ee3b1136abd11dfe0d
galcev <dengalcev@gmail.com>
2019-05-13 17:55:06 +0300

    Figures
    Version 1.0

---
+++ TitlesFrame.java
@@ -0,0 +1,36 @@
+/*    */ import javax.swing.JFrame;
+/*    */ 
+/*    */ public class TitlesFrame extends JFrame
+/*    */ {
+/*    */   public TitlesFrame()
+/*    */   {
+/*  7 */     initUI();
+/*    */   }
+/*    */   
+/*    */   private void initUI()
+/*    */   {
+/* 12 */     setTitle("Кривые фигуры");
+/* 13 */     setDefaultCloseOperation(3);
+/* 14 */     add(new TitlesPanel(78));
+/* 15 */     setSize(350, 350);
+/* 16 */     setLocationRelativeTo(null);
+/*    */   }
+/*    */   
+/*    */   public static void main(String[] args)
+/*    */   {
+/* 21 */     javax.swing.SwingUtilities.invokeLater(new Runnable()
+/*    */     {
+/*    */       public void run()
+/*    */       {
+/* 25 */         TitlesFrame ps = new TitlesFrame();
+/* 26 */         ps.setVisible(true);
+/*    */       }
+/*    */     });
+/*    */   }
+/*    */ }
+
+
+/* Location:              C:\Users\drago\Downloads\lab3.jar!\TitlesFrame.class
+ * Java compiler version: 7 (51.0)
+ * JD-Core Version:       0.7.1
+ */
